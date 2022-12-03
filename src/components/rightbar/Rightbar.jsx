c7ed9de7ccef66295110998e3e2ac31c2c307f82
khaled Reza <khaledreza@gmail.com>
2022-12-03 01:32:02 +0100

    Home Rightbar UI developed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,57 @@
+import { CardGiftcard } from "@material-ui/icons";
 import "./rightbar.css";
 
 const Rightbar = () => {
-  return <div className="rightbarContainer">Rightbar</div>;
+  return (
+    <div className="rightbarContainer">
+      <div className="birthdayNotification">
+        <CardGiftcard />
+        <span className="birthdayText">
+          <b>Pola Folster</b> and <b>3 friends</b> have birthday today
+        </span>
+      </div>
+      <img src="/assets/ad.png" alt="" className="rightbarAddImg" />
+      <div className="rightbarOnlineFriendList">
+        <span className="rightbarOnlineFriendTitle">Online Friends</span>
+        <div className="rightbarOnlineFriendItem">
+          <img
+            src="/assets/person/2.jpeg"
+            alt=""
+            className="rightbarOnlineFriendImg"
+          />
+          <div className="rightbarOnlineBadge"></div>
+          <span className="rightbarOnlineName">Janell Schrum</span>
+        </div>
+        <div className="rightbarOnlineFriendItem">
+          <img
+            src="/assets/person/2.jpeg"
+            alt=""
+            className="rightbarOnlineFriendImg"
+          />
+          <div className="rightbarOnlineBadge"></div>
+          <span className="rightbarOnlineName">Khaled Reza</span>
+        </div>
+        <div className="rightbarOnlineFriendItem">
+          <img
+            src="/assets/person/2.jpeg"
+            alt=""
+            className="rightbarOnlineFriendImg"
+          />
+          <div className="rightbarOnlineBadge"></div>
+          <span className="rightbarOnlineName">Taj Hashmi</span>
+        </div>
+        <div className="rightbarOnlineFriendItem">
+          <img
+            src="/assets/person/2.jpeg"
+            alt=""
+            className="rightbarOnlineFriendImg"
+          />
+          <div className="rightbarOnlineBadge"></div>
+          <span className="rightbarOnlineName">Kanak Sarwar</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
