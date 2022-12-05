a073fc5b00af45ce256053f0ad70d23d40dbd4d3
khaled Reza <khaledreza@gmail.com>
2022-12-06 00:10:12 +0100

    RegisterPage UI developed

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,40 @@
 import "./register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="registerContainer">
+      <div className="registerWrapper">
+        <div className="registerTextBlock">
+          <div className="registerLogo">Kaynat Social</div>
+          <div className="registerText">
+            Connect with friends and the world around you on Kaynat Social
+          </div>
+        </div>
+        <div className="registerInputBlock">
+          <div className="registerInputBox">
+            <input
+              className="registerInput"
+              type="text"
+              placeholder="username"
+            />
+            <input className="registerInput" type="text" placeholder="email" />
+            <input
+              className="registerInput"
+              type="text"
+              placeholder="password"
+            />
+            <input
+              className="registerInput"
+              type="text"
+              placeholder="confirm password"
+            />
+            <button className="inputButton">Sign Up</button>
+            <button className="createNewAccountButton">Log Into Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
