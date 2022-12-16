f28917d11df6b32722d642065f57ae5e6aaf95ab
khaled Reza <khaledreza@gmail.com>
2022-12-16 23:48:52 +0100

    test cicd

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -15,7 +15,9 @@ const Feed = () => {
             <span className="postUserId">genesisreza</span>
             <span className="postTimeAgo">1 hour ago</span>
           </div>
-          <div className="postStatusText">They are busy at studying</div>
+          <div className="postStatusText">
+            They are busy at studying (Changed here)
+          </div>
           <img src="/assets/post/6.jpeg" alt="" className="postItemImg" />
           <div className="postLikeComment">
             <div className="postLike">
